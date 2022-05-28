d239972a3323c385d7dcf5ffc2be55ef11003d54
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-05-29 02:41:10 +0530

    Contract V1 Delivered

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 
-
-contract Vault is ERC20, AccessControl, ReentrancyGuard {
+contract Vault is ReentrancyGuard, AccessControl {
     
     event OrderCreated(uint8 indexed orderId, uint64 indexed orderTime, OrderStatus indexed status);
     event OrderCancelled(uint8 indexed orderId, uint64 indexed orderTime, OrderStatus indexed status);
@@ -41,6 +41,8 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
         string termsOfCompletion;
         uint256 conflictPremium;
         bytes32 juryRoot;
+        uint64 juryNum;
+    
     }
 
     struct Conflict{
@@ -65,14 +67,14 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
     mapping(uint8 => Conflict) public conflicts;
 
 
-    constructor(address payable _seller, address payable _buyer) ERC20("Vault", "VLT"){
+    constructor(address payable _seller, address payable _buyer) {
         seller = _seller;
         buyer = _buyer;
         _setupRole(SELLER_ROLE, _seller);
         _setupRole(BUYER_ROLE, _buyer);
     }
 
-    function startNewOrder(string calldata termsURI, uint256 _conflictPremium, bytes32 _juryRoot) public payable onlyRole(BUYER_ROLE){
+    function startNewOrder(string calldata termsURI, uint256 _conflictPremium, bytes32 _juryRoot, uint64 _juryNum) public payable onlyRole(BUYER_ROLE){
         Order storage newOrder = orders[nextOrderId];
         newOrder.id = nextOrderId;
         newOrder.timeOfOrder = uint64(block.timestamp);
@@ -82,6 +84,7 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
         newOrder.termsOfCompletion = termsURI;
         newOrder.conflictPremium = _conflictPremium;
         newOrder.juryRoot = _juryRoot;
+        newOrder.juryNum = _juryNum;
 
         emit OrderCreated(nextOrderId, uint64(block.timestamp), OrderStatus.PROPOSED);
         nextOrderId++;
@@ -125,16 +128,21 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
 
     ///@param vote 0 for seller, 1 for buyer
     ///@param conflictId conflictId is always the same as orderId
-    function voteOnConflict(uint8 conflictId, bool vote) public{
+    function voteOnConflict(uint8 conflictId, bool vote, bytes32[] calldata addressProof) public{
         require(orders[conflictId].status == OrderStatus.CONFLICT, "Order must be in CONFLICT state");
         require(block.timestamp < conflicts[conflictId].conflictStartTime + resolutionTimePeriod, "Resolution Time Is Over");
         require(conflicts[conflictId].status == ConflictStatus.CONFLICT_IN_PROGRESS, "Conflict must be in CONFLICT_IN_PROGRESS state");
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(addressProof, orders[conflictId].juryRoot, leaf), "Address is not valid");
         if(vote){
             conflicts[conflictId].sellerVotes++;
         }
         else{
             conflicts[conflictId].buyerVotes++;
-        }    
+        }
+        uint payoutAmount = 2 * orders[conflictId].conflictPremium/ orders[conflictId].juryNum;
+        payable(address(msg.sender)).transfer(payoutAmount);
+
     }
 
     function provideConflictProofBuyer(uint8 conflictId,string memory conflictURI) public onlyRole(BUYER_ROLE){
@@ -144,10 +152,6 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
         conflicts[conflictId].sellerProof = conflictURI;
     }
 
-    function payConflictResolutionFee(uint8 orderId) internal {
-
-
-    }
 
     function settleConflict(uint8 orderId) public nonReentrant{
         require(orders[orderId].status == OrderStatus.CONFLICT, "Order must be in CONFLICT state");
@@ -167,16 +171,6 @@ contract Vault is ERC20, AccessControl, ReentrancyGuard {
             conflicts[orderId].status = ConflictStatus.CONFLICT_SETTLED_FOR_BUYER;
             buyer.transfer(orders[orderId].promisedAmount);
         }
-        payConflictResolutionFee(orderId);
-    }
-
-
-    function buyerCancelsOrder() public{
-
-    }
-
-    function sellerCancelsOrder() public{
-
     }
 
 }
