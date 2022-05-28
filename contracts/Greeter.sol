88c8b467fd6b1e8f805becace9278145dfc0782b
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-05-28 13:38:51 +0530

    Basic Structure of Contract

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,21 +2,79 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Greeter {
-    string private greeting;
+contract Vault is ERC20, AccessControl {
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    enum OrderStatus{
+        SETUP_COMPLETE,
+        ORDER_COMPLETE,
+        CONFLICT,
+        CANCELLED,
+        IN_PROGRESS,
+        NOT_ACTIVE
+    }
+    struct Order{
+        uint8 id;
+        bool sellerSetupComplete;
+        bool buyerSetupComplete;
+        uint64 timeOfOrder;
+        OrderStatus status;
+        uint256 promisedAmount;
+        uint256 paidAmount;
+        uint256 conflictPremium;
+        mapping(address => uint256) jury;
+        
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    bool private VaultSet = false;
+    address payable immutable seller;
+    address payable immutable buyer; 
+    bytes32 public constant SELLER_ROLE = keccak256("SELLER");
+    bytes32 public constant BUYER_ROLE = keccak256("BUYER");
+    mapping(uint8 => Order) public orders;
+
+    constructor(address payable _seller, address payable _buyer) ERC20("Vault", "VLT"){
+        seller = _seller;
+        buyer = _buyer;
+        _setupRole(SELLER_ROLE, _seller);
+        _setupRole(BUYER_ROLE, _buyer);
+
+    }
+    function startNewOrder() public{
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function requestPayout() public{
+
     }
+
+    function setJury() public{
+    }
+
+    function sellerSetup() public payable{
+
+    }
+
+    function buyerSetup() public payable{
+
+    }
+
+    // =============================== GETTERS ====================================
+    
+    // =============================== SETTERS ====================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
